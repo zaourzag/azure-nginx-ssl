ceedd708ea71edef9f48818f3754e797a75803e2
Zakaria aourzag <z.aourzag@gmail.com>
2022-04-01 07:29:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./src/server/app');
 const config = require('./config');
 
-const PORT = '80'
+const PORT = '8080'
 
 const server = app.listen(PORT,'0.0.0.0', () => console.log(`[Kubernetes] Probes API is listening on ${process.env.PORT}`));
 // eslint-disable-next-line no-unused-expressions
